1e3a5cdcf2c5a6b8420bb58225360cfc0d658ee6
Ankithirau <hirau.ankit888@gmail.com>
2023-05-01 13:41:50 +0530

    ui updates

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 
+const ejs= require('ejs');
+
 const app = express();
 
 const port=process.env.port || 3000;
 
 app.use('/static', express.static('views'));
 
+app.set('view engine','ejs');
+
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/views/index.html',(err)=> {
-        console.log(err);
-    });
+
+    res.render('index',{title:'Home',content:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit expedita quibusdam quasi earum sequi debitis quas. Numquam, libero quas! Dignissimos cumque assumenda tenetur illo recusandae. Assumenda aliquam iure dolore voluptatem!'});
+    // res.sendFile(__dirname + '/views/index.html',(err)=> {
+    //     console.log(err);
+    // });
 });
 
 app.get('/contact_us',(req,res)=>{
-    res.sendFile(__dirname + '/views/contact.html',(err)=> {
+    res.sendFile(__dirname + '/views/contact.ejs',(err)=> {
         console.log(err);
     });
 })
