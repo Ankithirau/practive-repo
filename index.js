3f7fa994aba0021856c6d7bfde925ec07cf7a7cf
Ankithirau <hirau.ankit888@gmail.com>
2023-05-01 12:42:58 +0530

    developing blog App

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-// const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use('/static', express.static('views'))
+const port=process.env.port || 3000;
 
+app.use('/static', express.static('views'));
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/index.html',(err)=> {
+    res.sendFile(__dirname + '/views/index.html',(err)=> {
+        console.log(err);
+    });
+});
+
+app.get('/contact_us',(req,res)=>{
+    res.sendFile(__dirname + '/views/contact.html',(err)=> {
         console.log(err);
     });
 })
 
-app.listen(3000,(req,res)=>{
-    console.log(`server is started at port ${ 3000 }`);
-})
+app.listen(port,(req,res)=>{
+    console.log(`server is started at port ${ port }`);
+});
 
-
-
-
+// const http = require('http');
 // const server = http.createServer((req, res) => {
 //     res.writeHead(200, { 'Content-Type': 'application/json' });
 //     res.end(JSON.stringify({
 //         data: 'hello world'
 //     }))
 // });
-
-
 // server.listen(3000);
-
-
