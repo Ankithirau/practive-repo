a94b78f7b0c1c847c80062f0ebf071ffb5b84982
Ankithirau <76995438+Ankithirau@users.noreply.github.com>
2023-06-04 15:09:48 +0000

    recent changes

--- type.js
+++ type.js
@@ -1,2 +1,4 @@
 var set = 25;
 console.log(set);
+var arr = ['hello', 25679];
+console.log(arr[1]);
--- type.ts
+++ type.ts
@@ -1,2 +1,5 @@
 let set:Number=25;
 console.log(set);
+let arr:[String,Number]=['hello',25679];
+console.log(arr[1]);
+
